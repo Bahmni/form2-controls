944c83377773ab1e1e22cf5d7a3c5efc1e86d239
Aishwarya-thoughtworks <aishwarya.sontakke@thoughtworks.com>
2026-06-07 21:32:29 +0530

    BAH-4767|Fix Jest configuration to properly handle Carbon package CSS imports

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '^src/(.*)$': '<rootDir>/src/$1',
     '^components/(.*)$': '<rootDir>/src/components/$1',
     '^helpers/(.*)$': '<rootDir>/src/helpers/$1',
+    '@carbon/styles/css/styles\\.css$': 'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'test-file-stub'
   },
